69995624ccbb452c7fecb55298ae5a61546fd567
Estêvão Ferreira <estevaofset@hotmail.com>
2025-01-22 15:22:35 -0300

    boilerplate inicial

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,37 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import { FlatCompat } from '@eslint/eslintrc'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: import.meta.dirname
+})
 
+/** @type {import('eslint').Linter.Config[]} */
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  ...compat.config({
+    extends: ['next/core-web-vitals']
+  }),
 
-export default eslintConfig;
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/prop-types': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off'
+    }
+  },
+
+  eslintConfigPrettier
+]
+
+export default eslintConfig
---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  testEnvironment: 'jsdom', //pois vamos estar rodando no browser
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'], //ignorar essas pastas
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    //quero que ele ignore esses de baixo
+    '!src/app/**',
+    '!src/lib/registry.tsx',
+    '!src/types/**',
+    '!src/**/stories.tsx',
+    '!src/styles/**'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/'], //ele vai pegar todos os arquivos da pasta src
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
+  moduleNameMapper: {
+    '^styled-components':
+      'styled-components/dist/styled-components.browser.cjs.js'
+  }
+}
